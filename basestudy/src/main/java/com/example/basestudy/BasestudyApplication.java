c98aa9c10fd751b6f115144b9081c6853c8d03de
ivan_liang <ivan_liang@sekorm.com>
2023-05-22 10:30:47 +0800

    搭建Java基础代码目录

---
+++ basestudy/src/main/java/com/example/basestudy/BasestudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.basestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasestudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasestudyApplication.class, args);
+    }
+
+}
---
+++ basestudy/src/test/java/com/example/basestudy/BasestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasestudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

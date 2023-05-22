b81e5456d0b7fefbda57b2217045fce3dba04c53
ivan_liang <ivan_liang@sekorm.com>
2023-05-22 10:37:30 +0800

    搭建各学习模块

--- basestudy/base/src/main/java/com/renchi/BaseApplication.java
+++ basestudy/base/src/main/java/com/renchi/BaseApplication.java
@@ -1,13 +1,17 @@
-package com.example.basestudy;
+package com.renchi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ivan_liang
+ * @describe
+ * @createTime 2023/05/22
+ */
 @SpringBootApplication
-public class BasestudyApplication {
+public class BaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasestudyApplication.class, args);
+        SpringApplication.run(BaseApplication.class, args);
     }
-
 }
